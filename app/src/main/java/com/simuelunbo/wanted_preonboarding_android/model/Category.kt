808bb3c093c7e46cfdd9680200a2ef7a6a22fbfa
simuelunbo <simuelunbo@gmail.com>
2022-09-14 16:16:42 +0900

    feat: category 모델 추가

---
+++ app/src/main/java/com/simuelunbo/wanted_preonboarding_android/model/Category.kt
@@ -0,0 +1,13 @@
+package com.simuelunbo.wanted_preonboarding_android.model
+
+import com.simuelunbo.wanted_preonboarding_android.R
+
+enum class Category(val image: Int) {
+    BUSINESS(R.drawable.ic_business),
+    ENTERTAINMENT(R.drawable.ic_entertainment),
+    GENERAL(R.drawable.ic_general),
+    HEALTH(R.drawable.ic_health),
+    SCIENCE(R.drawable.ic_science),
+    SPORTS(R.drawable.ic_sports),
+    TECHNOLOGY(R.drawable.ic_technology)
+}
